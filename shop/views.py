c0e15815cc3d79c8deb6a6ce6efb979f25e772fb
Joetib <otiboatengjoe@gmail.com>
2020-03-31 21:15:20 -0700

    Added initial pdf receit feature

--- shop/views.py
+++ shop/views.py
@@ -6,8 +6,35 @@ from django.contrib import messages
 from django.db.models import Q 
 from django.contrib.auth.decorators import login_required
 import json
-
+from io import BytesIO
+from django.template.loader import get_template
+from django.http import FileResponse, HttpResponse
+from xhtml2pdf import pisa
 # Create your views here.
+
+def render_to_pdf(template_src, context_dict={}):
+	template = get_template(template_src)
+	html  = template.render(context_dict)
+	result = BytesIO()
+	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+	if not pdf.err:
+		return HttpResponse(result.getvalue(), content_type='application/pdf')
+	return None
+
+data = {
+	"company": "Dennnis Ivanov Company",
+	"address": "123 Street name",
+	"city": "Vancouver",
+	"state": "WA",
+	"zipcode": "98663",
+
+
+	"phone": "555-555-2345",
+	"email": "youremail@dennisivy.com",
+	"website": "dennisivy.com",
+	}
+
+
 @login_required
 def add_product(request):
     if request.method == "POST":
@@ -43,24 +70,30 @@ def add_purchase(request):
         if form.is_valid():
             purchase = form.save()
             purchase_price = 0
+            
             for item in json.loads(request.POST.get('json_product_list')):
                 product = get_object_or_404(Product, pk=item.get('id'))
                 quantity = int(item.get("quantity", 0))
                 total_amount = float(product.price) * quantity
                 purchase_price += total_amount
                 
-                ItemPurchase.objects.create(
+                item_purchase = ItemPurchase.objects.create(
                     purchase = purchase,
                     quantity = item.get("quantity", 0),
                     product = product,
                     total_amount = total_amount
                 )
+            
             purchase.total_amount = purchase_price
             purchase.save()
+            
 
             messages.success(request, "Purchase created Successfully")
             redirect_url = request.GET.get("next")
-            print(redirect_url, '\n\n')
+      
+            pdf = render_to_pdf('pdf_template.html', {'purchase': purchase})
+            return HttpResponse(pdf, content_type='application/pdf')
+    
             if redirect_url is not None:
                 return redirect(redirect_url)
         else:
@@ -84,6 +117,7 @@ def history(request):
 
 @login_required
 def items_list(request):
+    
     items_list = Product.objects.all()
     
     search_query = request.GET.get('q')
